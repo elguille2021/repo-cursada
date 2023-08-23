ef8d91f2f02e759e6616ca43ee7100078a044035
elguille2021 <85086763+elguille2021@users.noreply.github.com>
2023-08-23 08:38:42 -0600

    Add files via upload
    Entregable #1 - Java

---
+++ NumeroFlotante_GuilleSerrano.java
@@ -0,0 +1,27 @@
+public class NumeroFlotante_GuilleSerrano
+ {
+    public static void main(String[] args) {
+        //psvm crea un main
+
+        int num=10;
+        float Numerodecim=num;   
+        System.out.println(num);
+        System.out.println(Numerodecim);
+
+
+
+
+    }
+}
+
+/* EXTRAE LA PARTE FLOTANTE DE UN ENTERO
+extraer o mostrar la parte entera de un numero   
+y su equivalente en float o decimal*/
+
+/*nota aqui no aplica esto pero repasando 
+lo de la 2da clase f al final para float no 10.5 sino 10.5f  ocupa 4 bytes
+y con long ocupa 8 bytes se pone una L */
+
+
+/*le puse engble1 "entregable1" 
+pero debe ser NumeroFlotante+GuilleSerrano.java  */ 
